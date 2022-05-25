c572cf9ada7fe0d5496b083869d4ca511503b239
wangyijie <371252847@qq.com>
2022-05-25 22:36:23 +0800

    2022/5/25 降雨预报大屏

--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ import 'element-plus/dist/index.css'
 import { dragMove, dragResize } from "./components/directives";
 import '@/css/font/font.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import dataV from '@jiaminghi/data-view'
+
+
+
+
 
 const app=createApp(App);
 app.use(ElementPlus);
@@ -18,3 +23,5 @@ app.use(router);
 app.mount("#app");
 app.use(dragMove); // 窗口拖动
 // app.use(dragResize); // 窗口缩放
+
+app.use(dataV)
